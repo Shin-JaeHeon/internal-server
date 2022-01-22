86cd7fe050272660ed2ecd76e490d00849f4ae05
Shin-JaeHeon <shinjaeheon520614@gmail.com>
2022-01-22 20:58:38 +0900

    BasicInternalServer 개선

--- src/server/basic/basicServer.ts
+++ src/server/basic/basicServer.ts
@@ -5,11 +5,23 @@ export class BasicInternalServer<Model extends BasicModelController> {
     constructor(private data: Model) {
     }
 
-    read<T, KEY_TYPE extends any>(key: KEY_TYPE): T {
+    get<T, KEY_TYPE extends any>(key: KEY_TYPE): T {
         return this.data.getData(key);
     }
 
+    read<T, KEY_TYPE extends any>(key: KEY_TYPE): T {
+        return this.get(key);
+    }
+
     set<T, KEY_TYPE extends any>(key: KEY_TYPE, data: T) {
-        return this.data.setData(key, data);
+        this.data.setData(key, data);
+    }
+
+    create<T, KEY_TYPE extends any>(key: KEY_TYPE, data: T) {
+        this.set(key, data);
+    }
+
+    update<T, KEY_TYPE extends any>(key: KEY_TYPE, data: T) {
+        this.set(key, data);
     }
 }
